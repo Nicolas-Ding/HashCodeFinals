1ef34acc345264f2c3cbe21f5e37d5f5978b8d93
RyZum <ryzumm@gmail.com>
2017-03-31 21:56:40 +0200

    INIT commit

---
+++ src/main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+
+public class main {
+	
+	final int threshStep = 5;
+
+	
+
+	public static void main(String[] args) throws IOException {
+		//String fichier = "me_at_the_zoo";
+		String fichier = "Kittens";
+		//String fichier = "trending_today";
+		//String fichier = "videos_worth_spreading";
+		// TODO Auto-generated method stub
+		Problem problem = Problem.read(fichier + ".in");
+		
+	}
+
+}
