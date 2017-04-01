aa15a608dec84cc3aa96468e288e4c9e8384d3db
Matei Oltean <matei.oltean@polytechnique.edu>
2017-04-01 11:52:03 +0200

    Adapted for Arrays of arrays

--- MapUtils.java
+++ MapUtils.java
@@ -4,7 +4,7 @@ public class MapUtils {
 		if (Math.abs(source.x-target.x) <= R && Math.abs(source.y-target.y) <= R) {
 			for (int i = Math.min(source.x, target.x); i <= Math.max(source.x, target.x); i++) {
 				for (int j = Math.min(source.y, target.y); j <= Math.max(source.y, target.y); j++) {
-					if (map.map.get(i).get(j).type == Case.MUR)
+					if (map.map[i][j].type == Case.MUR)
 						return false;
 				}
 			}
